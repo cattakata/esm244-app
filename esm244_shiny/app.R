acd04c66a29fe7d591cd846a859c570773416267
lorysalazar <lorysalazarvelasquez@gmail.com>
2021-02-08 21:19:47 -0800

    added select box

--- esm244_shiny/app.R
+++ esm244_shiny/app.R
@@ -1,49 +1,52 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+library(tidyverse)
 library(shiny)
+library(here)
 
-# Define UI for application that draws a histogram
+foraging <- read.csv(here("data","2018_foraging.csv"))
+
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+                
+                navbarPage("TITLE!!",
+                           tabPanel("Thing 1",
+                                    sidebarLayout(
+                                        sidebarPanel(selectInput("select", label = h3("Select dive type:"), 
+                                                                 choices = list("All dives" = 1, "Successful dives" = 2, "Unsuccessful dives" = 3), 
+                                                                 selected = 1),
+                                                     
+                                                     hr(),
+                                                     fluidRow(column(3, verbatimTextOutput("value")))
+                                                     
+                                                     
+                                                     
+                                                     ),
+                                        mainPanel("map coming soon")
+                                    )
+                                    ),
+                           tabPanel("Thing 2",
+                                    sidebarLayout(
+                                        sidebarPanel("widgets"),
+                                        mainPanel("output")
+                                    )
+                                    ),
+                           tabPanel("Thing 3",
+                                    sidebarLayout(
+                                        sidebarPanel("widgets"),
+                                        mainPanel("output")
+                                    )
+                                    ),
+                           tabPanel("Thing 4",
+                                    sidebarLayout(
+                                        sidebarPanel("widgets"),
+                                        mainPanel("output")
+                                    )
+                                    )
+                           
+                           
+                )
+                
+                
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output) {}
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
-}
-
-# Run the application 
 shinyApp(ui = ui, server = server)
